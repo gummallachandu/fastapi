0c68e926678796f6bb9834fa3cb7a794f0e01eb7
gummallachandu <gummalla.chandu@gmail.com>
2025-07-08 11:12:57 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from app.routers import file_reader, file_writer, jira_tool
+from app.routers import file_tool, jira_tool
 
 app = FastAPI()
 
-app.include_router(file_reader.router)
-app.include_router(file_writer.router)
+app.include_router(file_tool.router)
 app.include_router(jira_tool.router)
 
 
